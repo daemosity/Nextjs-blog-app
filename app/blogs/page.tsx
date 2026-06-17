de62217de38a24e40048162cb5eb2e010c3d24c6
Sean PC <sean.peckcollier@gmail.com>
2026-06-17 17:55:26 -0500

    Refactor: Added app/services/blogs
    Moved hardcoded blogs from app/blogs/page to services/blogs to prep for DB integration
    
    - Added functions  and

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,21 +1,8 @@
-const blogs = [
-  {
-    id: 1,
-    title: "First Blog Post",
-    author: "John Doe",
-    url: "/blogs/first-blog-post",
-    likes: 0,
-  },
-  {
-    id: 2,
-    title: "Second Blog Post",
-    author: "Jane Smith",
-    url: "/blogs/second-blog-post",
-    likes: 0,
-  },
-];
+import { getBlogs } from "../services/blogs";
 
 const Blogs = () => {
+  const blogs = getBlogs();
+
   return (
     <div>
       <h2>Blogs</h2>
---
+++ app/services/blogs.ts
@@ -0,0 +1,28 @@
+const blogs = [
+  {
+    id: 1,
+    title: "First Blog Post",
+    author: "John Doe",
+    url: "/blogs/first-blog-post",
+    likes: 0,
+  },
+  {
+    id: 2,
+    title: "Second Blog Post",
+    author: "Jane Smith",
+    url: "/blogs/second-blog-post",
+    likes: 0,
+  },
+];
+
+let nextId = 3;
+
+export const getBlogs = () => {
+    return blogs;
+}
+
+export const addBlog = (title: string, author: string, url: string) => {
+    const newBlog = { id: nextId++, title, author, url, likes: 0 };
+    blogs.push(newBlog);
+    return newBlog;
+}
