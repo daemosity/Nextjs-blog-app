ad2469f9b409d02aa202949695cd545edd9b2668
Sean PC <sean.peckcollier@gmail.com>
2026-06-17 20:00:40 -0500

    Feat: Connected Blogs List to individual Blogs Page

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -12,8 +12,15 @@ const BlogPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   return (
     <div className="max-w-4xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
-      <p className="text-lg text-gray-700">{blog.author}</p>
-      <p className="text-lg text-gray-700">{blog.url}</p>
+      <p className="text-lg text-gray-700">By {blog.author}</p>
+      <a
+        href={blog.url}
+        className="text-lg text-gray-700"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {blog.url}
+      </a>
       <p className="text-lg text-gray-700">{blog.likes} likes</p>
     </div>
   );
