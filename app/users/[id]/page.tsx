99baf5f61419ddfdf3d3dbc71b708bff0b19e1e6
Sean PC <sean.peckcollier@gmail.com>
2026-06-18 21:30:33 -0500

    Feat: added users/[id]/page, connected hardcoded 'db' via getUserById, not connected yet

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { notFound } from "next/navigation";
+import { getUserById } from "../../services/users";
+
+const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const user = getUserById(Number(id));
+
+  if (!user) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>{user.username}</h1>
+      <p>IRL Name: {user.name}</p>
+    </div>
+  );
+};
+
+export default UserPage;
