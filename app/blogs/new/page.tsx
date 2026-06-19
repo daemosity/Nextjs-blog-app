2986b7f0758a587742e9bd54310d36913e8f73bc
Sean PC <sean.peckcollier@gmail.com>
2026-06-19 14:34:55 -0500

    Feat: New blog data validation
    - added client-side html validation in blogs/new/page with arbitrary requirement for all entries to be at least 5 characters long

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -4,24 +4,44 @@ import { auth } from "@/auth";
 
 const NewBlog = async () => {
   const session = await auth();
+
   if (!session?.user?.email) {
     redirect("/login");
   }
+
   return (
     <div>
       <h2>Create New Blog</h2>
       <form action={createBlog}>
         <div>
           <label htmlFor="title">Title:</label>
-          <input type="text" name="title" placeholder="Title" />
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            required
+            minLength={5}
+          />
         </div>
         <div>
           <label htmlFor="author">Author:</label>
-          <input type="text" name="author" placeholder="Author" />
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            required
+            minLength={5}
+          />
         </div>
         <div>
           <label htmlFor="url">URL:</label>
-          <input type="text" name="url" placeholder="url" />
+          <input
+            type="text"
+            name="url"
+            placeholder="url"
+            required
+            minLength={5}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
