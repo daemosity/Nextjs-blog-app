2763ac82a371b30ccc65acf2c07b9c63ce21c9d4
Sean PC <sean.peckcollier@gmail.com>
2026-06-22 19:05:44 -0500

    Clean-up: components used by only one page moved to the page's respective directory

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 
-import NewBlogForm from "@/app/components/NewBlogForm";
+import NewBlogForm from "./NewBlogForm";
 
 const NewBlog = async () => {
   const session = await auth();
